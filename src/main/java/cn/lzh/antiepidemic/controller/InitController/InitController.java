8b2018697456eff8400789d1158c2e63fa50aed0
Administrator <1401383704@qq.com>
2021-12-15 13:20:33 +0800

    new controller

---
+++ src/main/java/cn/lzh/antiepidemic/controller/InitController/InitController.java
@@ -0,0 +1,13 @@
+package cn.lzh.antiepidemic.controller.InitController;
+
+/**
+ * []()
+ *
+ * @author LZH
+ * @version V1.0
+ * @description zh -
+ * @description en -
+ * @since 2021/12/15 13:19
+ */
+public class InitController {
+}
