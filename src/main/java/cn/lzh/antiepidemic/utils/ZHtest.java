3f17431c1356233afa68ede47576d3785296f2e1
ZH <2023037279@qq.com>
2021-12-16 10:08:08 +0800

    new ZHtest

---
+++ src/main/java/cn/lzh/antiepidemic/utils/ZHtest.java
@@ -0,0 +1,13 @@
+package cn.lzh.antiepidemic.utils;
+
+/**
+ * []()
+ *
+ * @author ZH
+ * @version v1.0
+ * @description zh -
+ * @description en -
+ * @since 2021/12/16 10:06
+ */
+public class ZHtest {
+}
